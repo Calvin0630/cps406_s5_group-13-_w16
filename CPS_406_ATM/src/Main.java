add431168187a6aea16be33db9bc896e960e116d
djack424 <djack424@users.noreply.github.com>
2016-03-18 01:08:59 -0400

    Update Main.java
    Added AccountDatabase class to hold client information

--- CPS_406_ATM/src/Main.java
+++ CPS_406_ATM/src/Main.java
@@ -1,10 +1,52 @@
-import javax.swing.*;
-import java.awt.*;
-
 public class Main {
 
     public static void main(String[] args) {
         ATM_GUI frame = new ATM_GUI();
 
     }
+}
+
+class AccountDatabase {
+	private double savingsBalance;
+	private double chequingBalance;
+	private int accountNumber;
+	private int PIN;
+	
+	public AccountDatabase (int newAccountNumber, int newPIN){
+		savingsBalance = chequingBalance = 0;
+		this.accountNumber = newAccountNumber;
+		this.PIN = newPIN;
+	}
+	
+	public AccountDatabase (){
+		return;
+	}
+	
+	public int getPIN (){
+		return PIN;
+	}
+	
+	public void setPIN(int newPIN){
+		PIN = newPIN;
+	}
+	
+	public int getAccountNumber (){
+		return accountNumber;
+	}
+	
+	public double getSavingsBalance(){
+		return savingsBalance;
+	}
+	
+	public void setSavingsBalance(double newBalance){
+		savingsBalance = newBalance;
+	}
+	
+	public double getChequingBalance(){
+		return chequingBalance;
+	}
+	
+	public void setChequingBalance(double newBalance){
+		chequingBalance = newBalance;
+	}
 }
