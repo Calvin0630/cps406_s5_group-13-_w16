0ae7782208942366c845c3c6ab5fcb3991fc61a3
djack424 <djack424@users.noreply.github.com>
2016-03-19 15:46:34 -0400

    Update ATM_GUI_Component.java

--- CPS_406_ATM/src/ATM_GUI_Component.java
+++ CPS_406_ATM/src/ATM_GUI_Component.java
@@ -7,17 +7,9 @@ import java.util.ArrayList;
  * Created by Lionel on 2016-03-10.
  */
 public class ATM_GUI_Component extends  JComponent{
-    private JButton[] numButtons;
+    
 
     public ATM_GUI_Component(){
-        this.
-        numButtons =  new JButton[10];
-        for (int i = 0; i < 10; i++){
-            numButtons[i] = new JButton(Integer.toString(i));
-            numButtons[i].setBackground(Color.blue);
-            numButtons[i].setVisible(true);
-            this.add(numButtons[i]);
-        }
         this.setSize(300, 400);
     }
 
@@ -29,8 +21,6 @@ public class ATM_GUI_Component extends  JComponent{
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        for (int i = 0; i < 10 ; i++) {
-            this.numButtons[i].paint(g);
-        }
+        
     }
 }
