73f890940a81dce62a8f2d695c18d1826437eae2
djack424 <djack424@users.noreply.github.com>
2016-03-21 20:20:39 -0400

    Updated drawing method.

--- CPS_406_ATM/src/Phone.java
+++ CPS_406_ATM/src/Phone.java
@@ -1,61 +1,61 @@
 import java.awt.* ;
-/**
- * 
- * @author Daniel
- *
- */
-public class Phone {
+import javax.swing.JComponent;
+
+public class Phone extends JComponent{
 	boolean isNFC;
 	int xPos;
 	int yPos;
-	int height;
-	int width;
+	int height = ATM_GUI.NUM_PAD_DIMENSION * 3/2 * 2/3;
+	int width = ATM_GUI.NUM_PAD_DIMENSION * 2/3;
 	String yesNFC = "NFC";
 	String noNFC = "Non-NFC";
+	int PIN, accountNumber;
 	Rectangle body;
-	
-	public Phone (boolean NFC, int size, int x, int y){
-		isNFC = NFC;
-		width = size*2/3;
-		height = size * 3/2 * 2/3;
+
+	public Phone (boolean NFC, int x, int y, int newPIN, int accntNmbr){
+		isNFC = NFC; 
 		xPos = x;
 		yPos = y;
+		PIN = newPIN;
+		accountNumber = accntNmbr;
 	}
-	
+
 	public boolean getNFC (){
 		return isNFC;
 	}
-	
+
 	public int getX(){
 		return xPos;
-			}
-	
+	}
+
 	public void setX(int x){
 		xPos = x;
 	}
-	
+
 	public int getY(){
 		return yPos;
 	}
-	
+
 	public void setY(int y){
 		yPos = y;
 	}
-	
-	public Shape getBounds (){
+
+	public Rectangle getBounds (){
 		return body.getBounds();
 	}
-	
+
 	public int getWidth(){
 		return width;
 	}
-	
+
 	public int getHeight (){
 		return height;
 	}
-	
-	public void draw(Graphics2D g2)
-    {
+
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.black);
 		body = new Rectangle ( xPos,yPos,width, height);
 		g2.draw(body);
 		g2.drawOval((int)(body.getX()+body.getWidth()/2-ATM_GUI.NUM_PAD_DIMENSION*1/20),(int)(body.getY()+body.getHeight() * 9/10), 
@@ -63,6 +63,6 @@ public class Phone {
 		String display = noNFC;
 		if (isNFC) display = yesNFC;
 		g2.drawString(display, (int) body.getX()+10, (int) (body.getY()+body.getHeight()/2));
-    }
+	}
 
 }
