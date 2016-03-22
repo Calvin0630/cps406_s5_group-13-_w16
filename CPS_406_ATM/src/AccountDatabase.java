03b6c847a0fa7b78c0070782801dafbc918ae10b
djack424 <djack424@users.noreply.github.com>
2016-03-22 11:45:23 -0400

    Update AccountDatabase.java

--- CPS_406_ATM/src/AccountDatabase.java
+++ CPS_406_ATM/src/AccountDatabase.java
@@ -1,14 +1,3 @@
-class AccountDatabase {
-	private double savingsBalance;
-	private double chequingBalance;
-	private int accountNumber;
-	private int PIN;
-
-	public AccountDatabase (int newAccountNumber, int newPIN){
-		savingsBalance = chequingBalance = 0;
-		this.accountNumber = newAccountNumber;
-		this.PIN = newPIN;
-	}
 public class AccountDatabase {
 	private double savingsBalance;
 	private double chequingBalance;
@@ -64,38 +53,5 @@ public class AccountDatabase {
 
 	public void setCreditDebt(double debt){
 		creditDebt = debt;
-	}
-}
-
-	public AccountDatabase (){
-		return;
-	}
-
-	public int getPIN (){
-		return PIN;
-	}
-
-	public void setPIN(int newPIN){
-		PIN = newPIN;
-	}
-
-	public int getAccountNumber (){
-		return accountNumber;
-	}
-
-	public double getSavingsBalance(){
-		return savingsBalance;
-	}
-
-	public void setSavingsBalance(double newBalance){
-		savingsBalance = newBalance;
-	}
-
-	public double getChequingBalance(){
-		return chequingBalance;
-	}
-
-	public void setChequingBalance(double newBalance){
-		chequingBalance = newBalance;
 	}
 }
