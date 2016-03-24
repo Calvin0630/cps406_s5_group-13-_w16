d1bc65b1f7e355cc631f30414cdd076dfbed184f
djack424 <djack424@users.noreply.github.com>
2016-03-24 16:34:20 -0400

    Update Cheque.java

--- CPS_406_ATM/src/Cheque.java
+++ CPS_406_ATM/src/Cheque.java
@@ -15,10 +15,12 @@ public class Cheque extends JComponent implements ATMMovableFields{
 	NumberFormat form= NumberFormat.getCurrencyInstance();
 	DateFormat df = new SimpleDateFormat("dd/MM/yy");
 	Date dateobj = new Date();
+	int chequeNum = 0;
 	public Cheque (double val, int x, int y){
 		value = val;
 		xPos = x;
 		yPos = y;
+		chequeNum++;
 	}
 
 
@@ -80,11 +82,18 @@ public class Cheque extends JComponent implements ATMMovableFields{
 		Font f = new Font("Times New Roman", Font.BOLD, fontSize);
 		g2.setFont(f);
 		Rectangle body = new Rectangle ( xPos,yPos,width, height);
+		g2.draw(body);
 		g2.setColor(Color.pink);
 		g2.fill(body);
 		g2.setColor(Color.black);
-		g2.drawString("Client name here.", xPos+width/10, yPos+height/3);
+		g2.drawString("Client name here.", xPos+width/20, yPos+height*1/10);
 		g2.drawString(form.format(getValue()), xPos+width*3/4, (int)body.getCenterY());
+		g2.drawLine(xPos+width/10, (int)body.getCenterY(), xPos+width*2/3, (int)body.getCenterY());
 		g2.drawString(df.format(dateobj), xPos+width*3/4, yPos + height/4);
+		g2.drawString(String.format("%011d", ATM_GUI.accountDatabase.getAccountNumber()) + ":"
+				+ String.format("%03d", chequeNum), 
+				xPos+width/20, yPos+height-width/20);
+		g2.drawLine(xPos+width/10, yPos+height*3/4, xPos+width*4/10, yPos+height*3/4);
+		g2.drawLine(xPos+width/2, yPos+height*3/4, xPos+width*9/10, yPos+height*3/4);
 	}
 }
