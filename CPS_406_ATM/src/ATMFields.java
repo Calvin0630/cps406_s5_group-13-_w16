d98dc65c3c5e3662d30b29bb1ddf51d31489b1d4
djack424 <djack424@users.noreply.github.com>
2016-03-26 00:48:05 -0400

    Update ATMFields.java
    Fixed placement of fields.

--- CPS_406_ATM/src/ATMFields.java
+++ CPS_406_ATM/src/ATMFields.java
@@ -10,8 +10,21 @@ class ATMFields extends JComponent {
 	public static Card debitCard;
 	public static Cash twentyBill;
 	public static Cheque cheque;
-	
+
 	public ATMFields (){
+		cashDispensor = new Rectangle((int) (ATM_GUI.FRAME_WIDTH/2),
+				ATM_GUI.numPad.getY() + ATM_GUI.NUM_PAD_DIMENSION/4 + 10,
+				(int)(ATM_GUI.screenSize.getHeight()*4/10), 
+				(ATM_GUI.NUM_PAD_DIMENSION)/2-5);
+		cardSlot = new Rectangle((int)(cashDispensor.getX()+cashDispensor.getWidth()-(ATM_GUI.screenSize.getHeight()*3/10)),
+				(int)(ATM_GUI.screenSize.getHeight()*3/10), 
+				(int)(ATM_GUI.screenSize.getHeight()*3/10), 
+				(int)(ATM_GUI.screenSize.getHeight()/10));
+		printer = new Rectangle((int)(cashDispensor.getX()+cashDispensor.getWidth()-(ATM_GUI.screenSize.getHeight()*3/10)),
+				(int)(ATM_GUI.screenSize.getHeight()*1/10), 
+				(int)(ATM_GUI.screenSize.getHeight()*3/10), 
+				(int)(ATM_GUI.screenSize.getHeight()/10));
+
 		nonNFCPhone = new Phone (false,ATM_GUI.FRAME_WIDTH-ATM_GUI.NUM_PAD_DIMENSION*3/2,
 				10,0,0);
 		NFCPhone = new Phone (true,ATM_GUI.FRAME_WIDTH-ATM_GUI.NUM_PAD_DIMENSION*3/4,
@@ -23,13 +36,10 @@ class ATMFields extends JComponent {
 				ATM_GUI.FRAME_HEIGHT*2/4+ATM_GUI.NUM_PAD_DIMENSION*1/10);
 		cheque = new Cheque(1906.55,ATM_GUI.FRAME_WIDTH-ATM_GUI.NUM_PAD_DIMENSION*5/3,
 				ATM_GUI.FRAME_HEIGHT*3/4);
+		setOpaque(false);
 	}
 
 	public void drawPrinter(Graphics2D g2){
-		printer = new Rectangle((int) ATM_GUI.screenSize.getWidth() / 2 + ATM_GUI.NUM_PAD_DIMENSION/10,
-				(int)(ATM_GUI.screenSize.getHeight()*1/10), 
-				(int)(ATM_GUI.screenSize.getHeight()*3/10), 
-				(int)(ATM_GUI.screenSize.getHeight()/10));
 		g2.draw(printer);
 		g2.fillRect((int)(printer.getX()+printer.getWidth()/4), 
 				(int)(printer.getY()+printer.getHeight()*2/3),
@@ -37,29 +47,21 @@ class ATMFields extends JComponent {
 		g2.drawString("Printer", (int)(printer.getX()+printer.getWidth()/4), 
 				(int)(printer.getY()+printer.getHeight()*2/3) - 10);
 	}
-	public void drawCardSlot(Graphics2D g2){
-		cardSlot = new Rectangle((int) ATM_GUI.screenSize.getWidth() / 2 + ATM_GUI.NUM_PAD_DIMENSION/10,
-				(int)(ATM_GUI.screenSize.getHeight()*3/10), 
-				(int)(ATM_GUI.screenSize.getHeight()*3/10), 
-				(int)(ATM_GUI.screenSize.getHeight()/10));
-		g2.draw(cardSlot);
-		g2.fillRect((int)(cardSlot.getX()+cardSlot.getWidth()/4), 
-				(int)(cardSlot.getY()+cardSlot.getHeight()*2/3),
-				(int)cardSlot.getWidth()/2, (int)cardSlot.getHeight()/4);
-		g2.drawString("Insert Card", (int)(cardSlot.getX()+cardSlot.getWidth()/4), 
-				(int)(cardSlot.getY()+cardSlot.getHeight()*2/3) - 10);
-	}
 	public void drawCashDispensor(Graphics2D g2){
-		cashDispensor = new Rectangle((int) (ATM_GUI.screenSize.getWidth() / 2 + ATM_GUI.NUM_PAD_DIMENSION/10 -ATM_GUI.screenSize.getHeight()/10),
-				ATM_GUI.numPad.getY() + ATM_GUI.NUM_PAD_DIMENSION/4 + 10,
-				(int)(ATM_GUI.screenSize.getHeight()*4/10), 
-				(ATM_GUI.NUM_PAD_DIMENSION)/2-5);
 		g2.draw(cashDispensor);
 		g2.fillRect((int)(cashDispensor.getX()+cashDispensor.getWidth()/8), 
 				(int)(cashDispensor.getY()+cashDispensor.getHeight()*2/3),
 				(int)cashDispensor.getWidth()*3/4, (int)cashDispensor.getHeight()/4);
 		g2.drawString("Cash Dispensor", (int)(cashDispensor.getX()+cashDispensor.getWidth()/4), 
 				(int)(cashDispensor.getY()+cashDispensor.getHeight()*2/3) - 10);
+	}
+	public void drawCardSlot(Graphics2D g2){
+		g2.draw(cardSlot);
+		g2.fillRect((int)(cardSlot.getX()+cardSlot.getWidth()/4), 
+				(int)(cardSlot.getY()+cardSlot.getHeight()*2/3),
+				(int)cardSlot.getWidth()/2, (int)cardSlot.getHeight()/4);
+		g2.drawString("Insert Card", (int)(cardSlot.getX()+cardSlot.getWidth()/4), 
+				(int)(cardSlot.getY()+cardSlot.getHeight()*2/3) - 10);
 	}
 	public void drawNFC(Graphics2D g2){
 		double r = cardSlot.getWidth()*2/3;
@@ -73,7 +75,6 @@ class ATMFields extends JComponent {
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-
 		g2.setColor(ATM_GUI.background);
 		g2.fillRect(0,0,(int)ATM_GUI.screenSize.getWidth(),(int)ATM_GUI.screenSize.getHeight());
 		g2.setColor(Color.black);
