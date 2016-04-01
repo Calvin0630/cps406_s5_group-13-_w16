0e70d88d4f9de69d848bd53b67a4f6adf3eda191
Daniel Jack <djack424@users.noreply.github.com>
2016-03-31 21:16:21 -0400

    Update Phone.java

--- CPS_406_ATM/src/Phone.java
+++ CPS_406_ATM/src/Phone.java
@@ -1,9 +1,14 @@
-import java.awt.* ;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.geom.RectangularShape;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.JComponent;
 
+/**
+ * 
+ * @author Group 13
+ * Phone class provides visual components as well as values held by the phone component.
+ */
 public class Phone extends JComponent implements ATMMovableFields{
 	boolean isNFC;
 	int xPos;
@@ -14,7 +19,14 @@ public class Phone extends JComponent implements ATMMovableFields{
 	String noNFC = "Non-NFC";
 	int PIN, accountNumber;
 
-
+	/**
+	 * Constructor initializing variables.
+	 * @param NFC boolean determining if phone is NFC enabled or not.
+	 * @param x value determining xPos value.
+	 * @param y value determining yPos value.
+	 * @param newPIN sets the PIN set on the phone.
+	 * @param accntNmbr sets the account number the phone is associated with.
+	 */
 	public Phone (boolean NFC, int x, int y, int newPIN, int accntNmbr){
 		isNFC = NFC; 
 		xPos = x;
@@ -23,43 +35,83 @@ public class Phone extends JComponent implements ATMMovableFields{
 		accountNumber = accntNmbr;
 	}
 
+	/**
+	 * Returns NFC boolean.
+	 * @return NFC variable.
+	 */
 	public boolean getNFC (){
 		return isNFC;
 	}
 
+	/**
+	 * Returns xPos variable.
+	 */
 	public int getX(){
 		return xPos;
 	}
-	
+
+	/**
+	 * Sets the xPos variable.
+	 * @param x value to set xPos
+	 */
 	public void setX(int x){
 		xPos = x;
 	}
 
+	/**
+	 * Returns yPos variable.
+	 */
 	public int getY(){
 		return yPos;
 	}
-	
+
+	/**
+	 * Sets the yPos variable.
+	 * @param y value to set xPos
+	 */
 	public void setY(int y){
 		yPos = y;
 	}
 
-	public int getPIN(){return PIN;}
+	/**
+	 * @return PIN variable of phone.
+	 */
+	public int getPIN(){
+		return PIN;
+	}
 
-	public int getAccountNumber(){return  accountNumber;}
+	/**
+	 * @return accountNumber of phone.
+	 */
+	public int getAccountNumber(){
+		return  accountNumber;
+	}
 
-
+	/**
+	 *  Returns the rectangle with the bounds of the phone.
+	 */
 	public Rectangle getBounds (){
 		return new Rectangle (xPos, yPos, width, height);
 	}
 
+	/**
+	 * Returns the width of the phone.
+	 */
 	public int getWidth(){
 		return width;
 	}
 
+	/**
+	 *  Returns the height of the phone.
+	 */
 	public int getHeight (){
 		return height;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
@@ -79,6 +131,9 @@ public class Phone extends JComponent implements ATMMovableFields{
 	}
 
 	@Override
+	/**
+	 *  Given a MouseEvent, the phone is moved accordingly.
+	 */
 	public void moveField(MouseEvent event) {
 		if ((event.getPoint().getX() - width/2) > (ATM_GUI.FRAME_WIDTH/2)) {
 			setX((int)event.getPoint().getX()-width/2);
@@ -87,11 +142,17 @@ public class Phone extends JComponent implements ATMMovableFields{
 	}
 
 	@Override
+	/**
+	 *  Given a rectangle of the bounds of another item, determines if the bounds overlap.
+	 */
 	public boolean collides(RectangularShape item) {
 		return item.contains(getBounds());
 	}
 
 	@Override
+	/**
+	 * Given another object, determines if they containing the same information.
+	 */
 	public boolean equals(ATMMovableFields other) {
 		if(other.getClass() == getClass()){
 			Phone otherPhone = (Phone) other;
