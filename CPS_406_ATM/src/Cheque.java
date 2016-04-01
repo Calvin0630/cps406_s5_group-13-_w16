426f8795e768dc121c8ba1c3632171d4fec85f69
Daniel Jack <djack424@users.noreply.github.com>
2016-04-01 00:01:15 -0400

    Update Cheque.java
    Added javadoc comments

--- CPS_406_ATM/src/Cheque.java
+++ CPS_406_ATM/src/Cheque.java
@@ -6,9 +6,14 @@ import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-public class Cheque extends JComponent implements ATMMovableFields{
-	int width = ATM_GUI.NUM_PAD_DIMENSION*3/2, height = width/2;
 
+/**
+ * @author Group 13
+ * Cheque class provides visual components as well as values held by the phone component.
+ */
+public class Cheque extends JComponent implements ATMMovableFields{
+
+	int width = ATM_GUI.NUM_PAD_DIMENSION*3/2, height = width/2;
 	int xPos, yPos;
 	double value;
 
@@ -16,6 +21,13 @@ public class Cheque extends JComponent implements ATMMovableFields{
 	DateFormat df = new SimpleDateFormat("dd/MM/yy");
 	Date dateobj = new Date();
 	int chequeNum = 0;
+
+	/**
+	 * Constructor to initialize variables.
+	 * @param val double variable to indicate amount of cheque.
+	 * @param x int to set the x position.
+	 * @param y int to set the y position
+	 */
 	public Cheque (double val, int x, int y){
 		value = val;
 		xPos = x;
@@ -23,42 +35,70 @@ public class Cheque extends JComponent implements ATMMovableFields{
 		chequeNum++;
 	}
 
-
+	/**
+	 *  Returns xPos int of x position.
+	 */
 	public int getX(){
 		return xPos;
 	}
 
+	/**
+	 * Sets x position
+	 * @param x int to set xPos
+	 */
 	public void setX(int x){
 		xPos = x;
 	}
 
+	/**
+	 *  Returns yPos int of y position.
+	 */
 	public int getY(){
 		return yPos;
 	}
 
+	/**
+	 * Sets y position.
+	 * @param y int to set yPos
+	 */
 	public void setY(int y){
 		yPos = y;
 	}
 
+	/**
+	 * Returns the indicated value of the cheque.
+	 * @return int value
+	 */
 	public double getValue (){
-
 		return value;
 	}
 
+	/**
+	 *  Returns a rectangle representing the bounds of the cheque.
+	 */
 	public Rectangle getBounds (){
 		return new Rectangle (xPos, yPos, width, height);
 	}
 
+	/**
+	 *  Returns the width of the cheque.
+	 */
 	public int getWidth(){
 		return width;
 	}
 
-
+	/**
+	 *  Returns the height of the cheque.
+	 */
 	public int getHeight (){
 		return height;
 	}
 
 	@Override
+	/**
+	 * Using MouseEvent's positon, adjusts position of the cheque.
+	 * @param event is a MouseEvent containing point of mouse.
+	 */
 	public void moveField(MouseEvent event) {
 		if ((event.getPoint().getX() - width/2) > (ATM_GUI.FRAME_WIDTH/2)) {
 			setX((int)event.getPoint().getX()-width/2);
@@ -67,16 +107,28 @@ public class Cheque extends JComponent implements ATMMovableFields{
 	}
 
 	@Override
+	/**
+	 * Given the Rectangular shape of an item, determines if the bounds overlap.
+	 * @param item is a RectangularShape object.
+	 */
 	public boolean collides(RectangularShape item) {
 		return item.contains(getBounds());
 	}
 
 	@Override
+	/**
+	 * Determines if object is equal to another object, which is never true.
+	 * @param other another ATMMoveableFields object.
+	 * @return false
+	 */
 	public boolean equals(ATMMovableFields other) {
 		return false;
 	}
 
-
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
